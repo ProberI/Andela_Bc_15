dcd3d8d67034ca9281f7ec7d9809e15eb20e43bf
PaulUpendo <lovepaul46@outlook.com>
2017-02-09 01:05:26 +0300

    Submission one: FindMissingNUmbers

---
+++ Day3/missingNumbers.py
@@ -0,0 +1,17 @@
+def find_missing(one, two):
+    missing = []
+    for n in one:
+        if n not in two:
+            missing.append(n)
+    for n in two:
+        if n not in one:
+            missing.append(n)
+
+    if len(missing) > 0:  # Length being less than 0 would mean no missing numbers
+        if len(missing) == 1:
+            return missing[0]
+        else:
+             return missing  # To return multiple numbers where more than one is missing
+    else:
+
+        return 0
